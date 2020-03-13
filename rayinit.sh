39e42bcd602bc0fbb27bce5b914ad00050e00200
Lukas Pfahler <lukas.pfahler@udo.edu>
2020-03-13 16:26:11 +0100

    rayinit

--- rayinit.sh
+++ rayinit.sh
@@ -2,7 +2,7 @@
 MASTER=$( hostname )
 PORT=6379
 ray start --head --redis-port=${PORT}
-SLAVES = "ls8gpu04 ls8gpu05 ls8gpu06 ls8gpu08 ls8gpu09"
-for DEVICE in $DEVICES; do
-    ssh ${DEVICE} "source /opt/anaconda3/bin/activate && conda activate ray && ray start --address=${MASTER}:${PORT}"
+SLAVES="ls8gpu04.local ls8gpu05.local ls8gpu06.local ls8gpu08.local ls8gpu09.local"
+for DEVICE in $SLAVES; do
+    ssh ${DEVICE} "source /opt/anaconda3/bin/activate && conda activate arxiv && ray stop && ray start --address=${MASTER}:${PORT}"
 done
