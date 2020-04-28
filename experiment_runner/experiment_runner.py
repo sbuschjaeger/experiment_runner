9d4ed356b0285bd6af7e2e9be829199e9cd0620f
sbuschjaeger <sebastian.buschjaeger@tu-dortmund.de>
2020-04-28 16:07:11 +0200

    Added an experimental implementation for post_pipelines

--- experiment_runner/experiment_runner.py
+++ experiment_runner/experiment_runner.py
@@ -11,11 +11,34 @@ import time
 import traceback
 import joblib
 
+from sklearn.pipeline import Pipeline 
+
 import numpy as np
 import ray
 
+# TODO Change function header to be nicer
+def store_model(previous_output, model, out_path = None, x_train = None, y_train = None, x_test = None, y_test = None):
+    if hasattr(model, "store"):
+        print("FOUND CUSTOM MODEL")
+        # Custom model
+        # TODO Remove dim options here
+        model.store("{}".format(out_path), dim=x_train[0].shape, name="model") 
+    else:
+        if isinstance(model, Pipeline) and hasattr(model.steps[-1], "store"):
+            print("FOUND PIPELINE WITH CUSTOM MODEL")
+            # SKLEARN PIPELINE With custom model
+            custom_model = model.steps.pop()
+            joblib.dump(model, "{}model_pipeline.pkl".format(out_path))
+
+            # TODO Remove dim options here
+            custom_model.store("{}".format(out_path), dim=x_train[0].shape, name="model")
+        else:
+            print("FOUND PIPELINE")
+            joblib.dump(model, "{}/model.pkl".format(out_path))
+            # SKLEARN MODEL OR PIPELINE
+
 @ray.remote
-def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_path, store, verbose):
+def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_path, verbose):
     def replace_objects(d):
         d = d.copy()
         for k, v in d.items():
@@ -50,10 +73,6 @@ def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_p
     def cfg_to_str(cfg):
         cfg = replace_objects(cfg.copy())
         return json.dumps(cfg, indent=4)
-
-    # def store_model(out_path):
-    #     # TODO IMPLEMENT
-    #     pass
 
     # getfullargspec does not handle inheritance correctly.
     # Taken from https://stackoverflow.com/questions/36994217/retrieving-arguments-from-a-class-with-multiple-inheritance
@@ -119,13 +138,13 @@ def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_p
             tmpcfg["seed"] = seed
             tmpcfg["out_path"] = out_path
 
-            pipeline = modelcfg.get("pipeline", None)
-            if pipeline:
+            pre_pipeline = modelcfg.get("pre_pipeline", None)
+            if pre_pipeline:
                 from sklearn.base import clone
                 from sklearn.pipeline import make_pipeline
                 # print(pipeline)
-                pipeline = make_pipeline(*[clone(p) for p in pipeline], "passthrough")
-            tmpcfg["pipeline"] = pipeline
+                pre_pipeline = make_pipeline(*[clone(p) for p in pre_pipeline], "passthrough")
+            tmpcfg["pipeline"] = pre_pipeline
 
             expected = {}
             for key in get_ctor_arguments(model_ctor):
@@ -133,17 +152,18 @@ def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_p
                     expected[key] = tmpcfg[key]
 
             model = model_ctor(**expected)
-            if pipeline and "pipeline" not in expected:
+            if pre_pipeline and "pipeline" not in expected:
                 # Model cannot handle the pipeline internally,
                 # so we put the model at the end of the pipeline and
                 # train the whole pipeline instead of the model.
-                pipeline.steps[-1] = (model.__class__.__name__, model)
-                model = pipeline
+                pre_pipeline.steps[-1] = (model.__class__.__name__, model)
+                model = pre_pipeline
 
             start_time = time.time()
             model.fit(x_train, y_train)
             fit_time = time.time() - start_time
 
+            # TODO MAYBE ADD METRICS TO POST_PIPELINE?
             scores["fit_time"].append(fit_time)
             for name, fun in metrics.items():
                 if x_train is not None and y_train is not None:
@@ -152,29 +172,28 @@ def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_p
                 if x_test is not None and y_test is not None:
                     scores[name + "_test"].append(fun(model, x_test, y_test))
 
-            if store:
-                # raise NotImplementedError("Storing not Supported with Ray")
+            post_pipeline = modelcfg.get("post_pipeline", None)
+            if post_pipeline is None:
+                post_pipeline = []
+            
+            if verbose:
+                print("RUNNING POST_PIPELINE")
+            
+            previous_output = None
+            for step in post_pipeline:
                 if verbose:
-                    print("STORING")
+                    print("POST_PIPELINE STEP {}".format(step.__name__))
                 
-                if hasattr(model, "store"):
-                    print("FOUND CUSTOM MODEL")
-                    # Custom model
-                    # TODO Remove dim options here
-                    model.store("{}".format(out_path), dim=x_train[0].shape, name="model") 
-                else:
-                    if isinstance(model, pipeline) and hasattr(model.steps[-1], "store"):
-                        print("FOUND PIPELINE WITH CUSTOM MODEL")
-                        # SKLEARN PIPELINE With custom model
-                        custom_model = model.steps.pop()
-                        joblib.dump(model, "{}model_pipeline.pkl".format(out_path))
-
-                        # TODO Remove dim options here
-                        custom_model.store("{}".format(out_path), dim=x_train[0].shape, name="model")
-                    else:
-                        print("FOUND PIPELINE")
-                        joblib.dump(model, "{}/model.pkl".format(out_path))
-                        # SKLEARN MODEL OR PIPELINE
+                previous_output = step(
+                    previous_output, 
+                    model, 
+                    out_path, 
+                    x_train, 
+                    y_train, 
+                    x_test, 
+                    y_test
+                )
+            
         for score in list(scores.keys()):
             if len(scores[score]) > 1:
                 scores["mean_"+ score] = np.array(scores[score]).mean()
@@ -249,7 +268,6 @@ def run_experiments(basecfg, models, **kwargs):
                 experiment_id,
                 no_runs,
                 modelcfg.get("out_path", ".") + "/{}".format(experiment_id),
-                modelcfg.get("store", False),
                 modelcfg.get("verbose", False)
             ) for experiment_id, modelcfg in enumerate(models)
         ]
