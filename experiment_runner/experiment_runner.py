bb4659537d81c39782a7fe43dbd635ab164a8fee
sbuschjaeger <sebastian.buschjaeger@tu-dortmund.de>
2020-05-12 13:09:00 +0200

    Added cloudpickle for serialization of models Moved some functions to Utils.py for readability Added max_calls=1 to decorater to force ray to reload import paths

--- experiment_runner/experiment_runner.py
+++ experiment_runner/experiment_runner.py
@@ -2,86 +2,21 @@ import copy
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from functools import partial
-import inspect
 import json
 import os
 import smtplib
 import socket
 import time
 import traceback
-import joblib
-
-from sklearn.pipeline import Pipeline 
 
 import numpy as np
 import ray
 
-# TODO Change function header to be nicer
-def store_model(previous_output, model, out_path = None, x_train = None, y_train = None, x_test = None, y_test = None):
-    if hasattr(model, "store"):
-        print("FOUND CUSTOM MODEL")
-        # Custom model
-        # TODO Remove dim options here
-        model.store("{}".format(out_path), dim=x_train[0].shape, name="model") 
-    else:
-        if isinstance(model, Pipeline) and hasattr(model.steps[-1], "store"):
-            print("FOUND PIPELINE WITH CUSTOM MODEL")
-            # SKLEARN PIPELINE With custom model
-            custom_model = model.steps.pop()
-            joblib.dump(model, "{}model_pipeline.pkl".format(out_path))
+from experiment_runner.Utils import stacktrace, cfg_to_str, get_ctor_arguments, replace_objects
 
-            # TODO Remove dim options here
-            custom_model.store("{}".format(out_path), dim=x_train[0].shape, name="model")
-        else:
-            print("FOUND PIPELINE")
-            joblib.dump(model, "{}/model.pkl".format(out_path))
-            # SKLEARN MODEL OR PIPELINE
-
-@ray.remote
+# Force every worker to reload their import path by terminating each worker after each run,  see https://github.com/ray-project/ray/issues/6449
+@ray.remote(max_calls=1)
 def eval_model(modelcfg, metrics, get_split, seed, experiment_id, no_runs, out_path, verbose):
-    def replace_objects(d):
-        d = d.copy()
-        for k, v in d.items():
-            if isinstance(v, dict):
-                d[k] = replace_objects(v)
-            elif isinstance(v, list):
-                d[k] = [replace_objects({"key":vv})["key"] for vv in v]
-            elif isinstance(v, np.generic):
-                d[k] = v.item()
-            elif isinstance(v, partial):
-                d[k] = v.func.__name__ + "_" + "_".join([str(arg) for arg in v.args]) + str(replace_objects(v.keywords))
-            elif callable(v) or inspect.isclass(v):
-                try:
-                    d[k] = v.__name__
-                except:
-                    d[k] = str(v) #.__name__
-            elif isinstance(v, object) and v.__class__.__module__ != 'builtins':
-                # print(type(v))
-                d[k] = str(v)
-            else:
-                d[k] = v
-        return d
-
-    def stacktrace(exception):
-        """convenience method for java-style stack trace error messages"""
-        import sys
-        import traceback
-        print("\n".join(traceback.format_exception(None, exception, exception.__traceback__)),
-            #file=sys.stderr,
-            flush=True)
-
-    def cfg_to_str(cfg):
-        cfg = replace_objects(cfg.copy())
-        return json.dumps(cfg, indent=4)
-
-    # getfullargspec does not handle inheritance correctly.
-    # Taken from https://stackoverflow.com/questions/36994217/retrieving-arguments-from-a-class-with-multiple-inheritance
-    def get_ctor_arguments(clazz):
-        args = ['self']
-        for C in clazz.__mro__:
-            if '__init__' in C.__dict__:
-                args += inspect.getfullargspec(C).args[1:]
-        return args
     try:
         # Make a copy of the model config for all output-related stuff
         # This does not include any fields which hurt the output (e.g. x_test,y_test)
@@ -258,18 +193,19 @@ def run_experiments(basecfg, models, **kwargs):
                 if cfg not in model_cfg:
                     model_cfg[cfg] = basecfg[cfg]
 
-        futures = [eval_model.options(
-                num_cpus=modelcfg.get("num_cpus", 1),
-                num_gpus=modelcfg.get("num_gpus", 1)).remote(
-                modelcfg,
-                modelcfg["scoring"],
-                partial(get_train_test, basecfg=modelcfg),
-                seed,
-                experiment_id,
-                no_runs,
-                modelcfg.get("out_path", ".") + "/{}".format(experiment_id),
-                modelcfg.get("verbose", False)
-            ) for experiment_id, modelcfg in enumerate(models)
+        futures = [eval_model.options( 
+                    num_cpus=modelcfg.get("num_cpus", 1),
+                    num_gpus=modelcfg.get("num_gpus", 1)
+                ).remote(
+                    modelcfg,
+                    modelcfg["scoring"],
+                    partial(get_train_test, basecfg=modelcfg),
+                    seed,
+                    experiment_id,
+                    no_runs,
+                    modelcfg.get("out_path", ".") + "/{}".format(experiment_id),
+                    modelcfg.get("verbose", False)
+                ) for experiment_id, modelcfg in enumerate(models)
         ]
         total_no_experiments = len(futures)
         total_id = 0
